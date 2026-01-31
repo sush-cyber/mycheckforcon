b65d49aac320ac512a70cc92cca23105e09b6f04
sush-cyber <msushmitha998@gmail.com>
2026-01-31 21:51:16 +0530

    checking

---
+++ functions.js
@@ -0,0 +1,9 @@
+function isOddOrEven(number) {
+  if (number % 2 === 0) {
+    console.log('even');
+    return 'even';
+  } else {
+    console.log('odd');
+    return 'odd';
+  }
+}
---
+++ string.js
@@ -0,0 +1,2 @@
+console.log('Your Name');
+console.log('Hello, welcome to the program!');
